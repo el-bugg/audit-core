b1a3785c9159c04cc5368821cc3d9e50995e062e
zakialdrin <ahmdzaki249@gmail.com>
2025-12-30 07:56:20 +0700

    Fix Laravel paths in api/index.php

--- api/index.php
+++ api/index.php
@@ -4,14 +4,14 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+// 1. Cek mode maintenance (Arahkan ke folder coin-web)
+if (file_exists($maintenance = __DIR__.'/../coin-web/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/../../vendor/autoload.php';
+// 2. Register Autoloader (Arahkan ke folder coin-web/vendor)
+require __DIR__.'/../coin-web/vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../../bootstrap/app.php')
+// 3. Bootstrap Laravel (Arahkan ke folder coin-web/bootstrap)
+(require_once __DIR__.'/../coin-web/bootstrap/app.php')
     ->handleRequest(Request::capture());
