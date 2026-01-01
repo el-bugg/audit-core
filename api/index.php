7b7a33d17ec134b1d1815f8eb5b089079090ac01
el-bugg <zakialdrin@gmail.com>
2026-01-01 19:54:16 +0700

    Fix: move vendor inside api folder for vercel compatibility

--- api/index.php
+++ api/index.php
@@ -2,7 +2,7 @@
 // Cek beberapa kemungkinan lokasi vendor
 // Sesuaikan dengan struktur folder di VS Code Anda
 if (file_exists(__DIR__ . '/../coin-web/vendor/autoload.php')) {
-    require __DIR__ . '/../coin-web/vendor/autoload.php';
+    require __DIR__ . '/vendor/autoload.php';
 } else {
     echo "Vendor autoload tidak ditemukan. Pastikan composer install sudah dijalankan.";
 }
